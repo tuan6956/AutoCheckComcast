c347c960c8c6b79236fa0676bfce492eb680456c
tuan6956 <ng.anhtuan20021997@gmail.com>
2017-11-01 17:33:42 +0700

    Add project files.

---
+++ AutoCheckComcast/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using AutoIt;
+using System.Diagnostics;
+
+namespace AutoCheckComcast
+{
+    public partial class Form1 : Form
+    {
+        private int CountFile = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openfile = new OpenFileDialog();
+            openfile.Filter = "txt files (*.txt)|*.txt";
+            openfile.FilterIndex = 3;
+            openfile.RestoreDirectory = true;
+            if (openfile.ShowDialog() == DialogResult.OK)
+            {
+                tbPath.Text = openfile.FileName;
+                string[] accounts = File.ReadAllLines(tbPath.Text);
+                List<string> list = new List<string>();
+                for (int i = 0; i < accounts.Length; i++)
+                {
+                    if (i % 100 == 0 && i != 0)
+                    {
+                        CountFile++;
+                        string[] temp = list.ToArray();
+                        File.WriteAllLines($"account/account{CountFile}.txt", temp);
+                        list.Clear();
+                    }
+                    else if (accounts.Length - i == 1)
+                    {
+                        list.Add(accounts[i]);
+                        CountFile++;
+                        string[] temp = list.ToArray();
+                        File.WriteAllLines($"account/account{CountFile}.txt", temp);
+                        list.Clear();
+                        break;
+                    }
+                    list.Add(accounts[i]);
+
+                }
+            }
+            
+        }
+        private void killAPP()
+        {
+            Process[] processesByName = Process.GetProcessesByName("CheckLiveComcast");
+            foreach (Process process in processesByName)
+            {
+                process.Kill();
+            }
+        }
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            btnCheck.Enabled = false;
+            AutoItX.WinActivate("Comcast");
+            for (int i = 1; i <= CountFile; i++)
+            {
+                Process pr = new Process();
+                pr.StartInfo.FileName = "CheckLiveComcast.exe";
+                pr.Start();
+                await Task.Delay(1000);
+                AutoItX.WinActivate("Comcast");
+                AutoItX.ControlSetText("Comcast", "", "[NAME:tbPath]", i.ToString());
+                await Task.Delay(1000);
+                AutoItX.ControlClick("Comcast", "", "[NAME:btnStart]");
+                AutoItX.WinSetTitle("Comcast", "", i.ToString());
+                if (i % 5 == 0 || i == CountFile) 
+                {
+                    await Task.Delay(20000);
+                    killAPP();
+                }
+                await Task.Delay(1000);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText("data/Live.txt", null);
+            MessageBox.Show("Clear success!");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] filePaths = Directory.GetFiles("account/");
+            foreach (string filePath in filePaths)
+                File.Delete(filePath);
+        }
+    }
+}
---
+++ AutoCheckComcast/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AutoCheckComcast")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AutoCheckComcast")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("dd2814c1-49ff-4534-9d52-70de32e637ee")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
